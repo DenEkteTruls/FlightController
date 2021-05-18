1d1b33d4785a5e87a49820ef47ba02ecf16266fb
AutomaticBirdPhotography <79084533+AutomaticBirdPhotography@users.noreply.github.com>
2021-05-18 18:42:54 +0200

    Update GPSvinkel.ino
    Funker trolig nå

--- GPSvinkel.ino
+++ GPSvinkel.ino
@@ -1,40 +1,41 @@
 /*
       Author: Robert Brenner Marhtins
-      Version: 0.1.00
+      Version: 0.2.00
       Comments:
-
+      Virker til at alt skal fungere etter som stikkprøver v/ sammenlikning til python ga tilnærmet like resultater
 */
 float flyLat = 61;
 float flyLng = 10;
 float flyAlt = 0;
 
 float punktLat = 60;
-float punktLng = 10;
-float punktAlt = 0;
+float punktLng = 11;
+float punktAlt = 100;
 
 void setup() {
-
+Serial.begin(9600);
 
 }
 
 void loop() {
-  Serial.println(geoVinkel(flyLat, flyLng, punktLat, punktLng, flyAlt, punktAlt));
+  geoVinkel(flyLat, flyLng, punktLat, punktLng, flyAlt, punktAlt);
   delay(1000);
 
 }
 
-float geoVinkel(lat1, long1, lat2, long2, alt1, alt2) {
+void geoVinkel(float lat1, float long1, float lat2, float long2, float alt1, float alt2) {
   float dN = lat2 - lat1;
   float dE = long2 - long1;
-  float r = 6371 * 1000;
+  unsigned long r = 6371000;
   float r2 = r * cos(lat1 * PI / 180);
-  float dNm = sqrt(2 * (r ^ 2) - 2 * (r ^ 2) * cos(dN * PI / 180));
-  float dEm = sqrt(2 * (r2 ^ 2) - 2 * (r2 ^ 2) * cos(dE * PI / 180));
+  float dNm = sqrt(2 * (pow(r,2)) - 2 * (pow(r,2)) * cos(dN * PI / 180));
+  float dEm = sqrt(2 * (pow(r2,2)) - 2 * (pow(r2,2)) * cos(dE * PI / 180));
   float dH = alt2 - alt1;
   if (dEm == 0) {
     dEm = 0.000000000001;
   }
   float Vh1 = atan(dNm / dEm) * 180 / PI;
+  float Vh = 0;
 
   if (dN >= 0) {
     if (dE >= 0) {
@@ -52,9 +53,13 @@ float geoVinkel(lat1, long1, lat2, long2, alt1, alt2) {
       Vh = 270 - Vh1;
     }
   }
-  float L = sqrt(dNm ^ 2 + dEm ^ 2);
-  float Al = sqrt(L ^ 2 + dH ^ 2);
-  float Vv = atan(dH / L) * 180 / pi;
+  float L = sqrt(pow(dNm,2) + pow(dEm,2));
+  float Al = sqrt(pow(L,2) + pow(dH,2));
+  float Vv = atan(dH / L) * 180 / PI;
 
-  return Al, Vv, Vh;
+ // return Al, Vv, Vh;
+ Serial.println(Al); // Avstand
+ Serial.println(Vv); // Vinkel vertikal
+ Serial.println(Vh); // Vinkel horisontal
+ Serial.println("\n");
 }
