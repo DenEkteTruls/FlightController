198ba03018e2ede3456f72b0f24041f170c51a3b
DenEkteTruls <44004719+DenEkteTruls@users.noreply.github.com>
2021-05-20 12:01:12 +0200

    Update plane_stabilizer.ino

--- plane_stabilizer.ino
+++ plane_stabilizer.ino
@@ -1,16 +1,10 @@
-/*
-      Author: Robert Brenner Marhtins
-      Version: 0.1.00
-      Comments:
-
-*/
-
 #include "MPU9250.h"
 #include "Servo.h"
 
 MPU9250 mpu;
 Servo sideror;
 int pitch_v_max_servo = 45;
+int yaw_v_max_servo 
 float turn_angle = 0;
 
 void setup() {
@@ -20,7 +14,7 @@ void setup() {
   Wire.begin();
   delay(2000);
 
-  if (!mpu.setup(0x68)) {  // change to your own address
+  if (!mpu.setup(0x68)) {
     while (1) {
       Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
       delay(5000);
@@ -31,22 +25,23 @@ void setup() {
 void loop() {
   if (mpu.update()) {
     static uint32_t prev_ms = millis();
-    if (millis() > prev_ms + 25) {
-      turn();
+    if(millis() - prev_ms >= 25) {
       prev_ms = millis();
+      stabilize();
     }
   }
 }
 
-void turn() {
-  //Serial.print("Yaw, Pitch, Roll: ");
-  //Serial.print(mpu.getYaw(), 2);
-  //Serial.print(", ");
+void stabilize() {
+  //Serial.print("Yaw, Pitch, Roll: "); //
+  Serial.print(mpu.getYaw(), 2);
+  //Serial.print(", ");                 //
   Serial.println(mpu.getPitch(), 2);
-  //Serial.print(", ");
-  //Serial.println(mpu.getRoll(), 2);
+  //Serial.print(", ");                 //
+  //Serial.println(mpu.getRoll(), 2);   //
   
   float servo_pos = map(mpu.getPitch(), -pitch_v_max_servo - turn_angle, pitch_v_max_servo - turn_angle, 0, 180);
+  float elevator_pos = map(mpu.getYaw(), yaw_v_mac_servo - yaw_angle, -yaw_v_max_servo - yaw_angle, 0, 180);
   Serial.println(servo_pos);
   sideror.write(servo_pos);
 }
